a45636e058cc9bb1384ac960d464e368e433e190
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-27 12:41:09 -0500

    label

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 import tkinter
 
 window = tkinter.Tk()
+window.title("First Title")
+window.minsize(width=500, height=300)
+
+#Label
+my_label = tkinter.Label(text="First Label", font=("Arial", 24, "bold"))
+
+my_label.pack(expand=True)
+#my_label.pack() default to top center, side="left" is left center
+
+
+
+
+
+window.mainloop()    # keeps on screen
