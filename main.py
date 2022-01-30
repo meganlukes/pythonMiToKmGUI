e73882511e8d16ef1e935cb724235edefa5776ee
Megan Lukes@gmail.com <meglukes@gmail.com>
2022-01-30 18:45:04 -0500

    radio buttons

--- main.py
+++ main.py
@@ -81,6 +81,18 @@ checked_state.get()
 checkbutton.pack(expand=True)
 
 
+def radio_used():
+   print(radio_state.get())
+
+
+# Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack(expand=True)
+radiobutton2.pack(expand=True)
+
+
 
 window.mainloop()    # keeps on screen
 
